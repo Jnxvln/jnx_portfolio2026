76e074a40a56d8575ebc1dfa6a68006aef6b6f39
Justin Cox <jnx1017@gmail.com>
2026-07-20 16:59:12 -0500

    Initial project cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,10 @@
 import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>Justin Cox | Portfolio (2026)</Title>
+      <h1 class="font-bold">Justin Cox Portfolio</h1>
     </main>
   );
 }
